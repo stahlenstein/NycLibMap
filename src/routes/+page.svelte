ddaa9dbb069d0a47bf5efb588743ffae6bee8eb8
stahlenstein <stahlstradamus@gmail.com>
2023-10-16 18:27:10 -0400

    gh-pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
     import LeafletMap from '$lib/LeafletMap.svelte';
+    import { base } from '$app/paths';
 </script>
+
+<nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+</nav>
 
 <main>
     <LeafletMap />
