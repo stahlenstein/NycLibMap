55f641f614f82f485c1ec4a455a2a0a8a5d20af0
stahlenstein <stahlstradamus@gmail.com>
2023-10-16 18:44:26 -0400

    updated script

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import LeafletMap from '$lib/LeafletMap.svelte';
+    import { base } from '$app/paths';
+</script>
+
+<nav>
+    <!-- <a href="{base}/">Home</a> -->
+    <!-- <a href="{base}/about">About</a> -->
+</nav>
+
+<main>
+    <LeafletMap />
+</main>
