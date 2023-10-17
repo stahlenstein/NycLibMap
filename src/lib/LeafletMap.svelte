7e6de537a0537160bdd2129a14dc212730253a33
stahlenstein <stahlstradamus@gmail.com>
2023-10-16 20:38:21 -0400

    Added indoor js info

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -23,26 +23,29 @@
 		if (browser) {
 			// add await imports //
 			L = await import('leaflet');
+			await import('static/Plugins/leaflet-indoor/leaflet-indoor.js');
 
-			map = L.map(mapElement, { zoomControl: true, maxZoom: 18, minZoom: 11 }).setView(
+			map = L.map(mapElement, { zoomControl: true, maxZoom: 22, minZoom: 11 }).setView(
 				[40.753322, -73.982544],
 				11
 			);
 			var Esri_WorldTopoMap = L.tileLayer(
 				'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
 				{
+					maxZoom: 19,
 					attribution:
 						'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 				}
 			);
 
 			var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				maxZoom: 22,
 				attribution:
 					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			});
 
-			// osmLayer.addTo(map);
-			Esri_WorldTopoMap.addTo(map);
+			osmLayer.addTo(map);
+			// Esri_WorldTopoMap.addTo(map);
 
 			map.createPane('pane_Limits');
 
@@ -114,7 +117,21 @@
 						pointToLayer: addLibIcon,
 						onEachFeature: function (feature, layer) {
 							layer.bindPopup(
-								'<h1>' + feature.properties.name + '</h1><p>system: ' + feature.properties.system + '</p>' + '<p>status: ' + feature.properties.status + '</p>' + '<p>url: ' + '<a href="' + feature.properties.url +'">' + feature.properties.url +'</a>' + '</p>'
+								'<h1>' +
+									feature.properties.name +
+									'</h1><p>system: ' +
+									feature.properties.system +
+									'</p>' +
+									'<p>status: ' +
+									feature.properties.status +
+									'</p>' +
+									'<p>url: ' +
+									'<a href="' +
+									feature.properties.url +
+									'">' +
+									feature.properties.url +
+									'</a>' +
+									'</p>'
 							);
 						}
 					};
@@ -128,6 +145,54 @@
 				});
 		}
 	});
+
+	// This example uses a GeoJSON FeatureCollection saved to a file
+	// (data.json), see the other example (live/index.html) for details on
+	// fetching data using the OverpassAPI (this is also how the data in
+	// data.json was generated)
+
+	// getJSON('data.json', function (geoJSON) {
+	// 	var indoorLayer = new L.Indoor(geoJSON, {
+	// 		getLevel: function (feature) {
+	// 			if (feature.properties.relations.length === 0) return null;
+
+	// 			return feature.properties.relations[0].reltags.level;
+	// 		},
+	// 		onEachFeature: function (feature, layer) {
+	// 			layer.bindPopup(JSON.stringify(feature.properties, null, 4));
+	// 		},
+	// 		style: function (feature) {
+	// 			var fill = 'white';
+
+	// 			if (feature.properties.tags.buildingpart === 'corridor') {
+	// 				fill = '#169EC6';
+	// 			} else if (feature.properties.tags.buildingpart === 'verticalpassage') {
+	// 				fill = '#0A485B';
+	// 			}
+
+	// 			return {
+	// 				fillColor: fill,
+	// 				weight: 1,
+	// 				color: '#666',
+	// 				fillOpacity: 1
+	// 			};
+	// 		}
+	// 	});
+
+	// 	indoorLayer.setLevel('0');
+
+	// 	indoorLayer.addTo(map);
+
+	// 	var levelControl = new L.Control.Level({
+	// 		level: '0',
+	// 		levels: indoorLayer.getLevels()
+	// 	});
+
+	// 	// Connect the level control to the indoor layer
+	// 	levelControl.addEventListener('levelchange', indoorLayer.setLevel, indoorLayer);
+
+	// 	levelControl.addTo(map);
+	// });
 
 	onDestroy(async () => {
 		if (map) {
